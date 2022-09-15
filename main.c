d0abc27564d0dc3d1cab1617dfd944a6ddd366fb
miaou33 <nfauconn@student.42.fr>
2022-09-15 23:33:34 +0200

    exam

--- main.c
+++ main.c
@@ -1,75 +1,17 @@
-#include "get_next_line_bonus.h"
+#include "get_next_line.h"
 
-static int	is_all_ended(int *end_fd, int nb_fd)
+int main()
 {
+	int fd1 = open("file1", O_RDONLY);
+	char *str = get_next_line(fd1);
 	int	i = 0;
-	while (i < nb_fd && end_fd[i])
-		i++;
-	if (i == nb_fd)
-		return (1);
-	else
-		return (0);
-}
 
-void	test_gnl(int nb_fd, char **av)
-{
-	int		fd[nb_fd];
-	int		end_fd[nb_fd];
-	int		line_no[nb_fd];
-	char	*line;
-	int		i = 0;
-	int		j = 0;
-	int		end_all_files = 0;
-
-	printf("\n");
-	while (i < nb_fd)
+	while (str)
 	{
-		fd[i] = open(av[i + 1], O_RDONLY);
-		if (fd[i] > 0)
-			printf("%s opened : fd%d\n", av[i + 1], fd[i]);
-		end_fd[i] = 0;
-		i++;
+		printf("line[%d] = %s\n", i++, str);
+		free(str);
+		str = get_next_line(fd1);
 	}
-	printf("\n");
-	while (!end_all_files)
-	{
-		int i = 0;
-		while (i < nb_fd)
-		{
-			if (!end_fd[i])
-			{
-				line = get_next_line(fd[i]);
-				printf("line[fd%d][%d] = %s", fd[i], j, line);
-				if (!line)
-				{
-					printf("\n");
-					end_fd[i] = 1;
-				}
-				else
-				{
-					free(line);
-					line = NULL;
-				}
-			}
-			i++;
-		}
-		printf("\n");
-		j++;
-		end_all_files = is_all_ended(end_fd, nb_fd);
-	}
-	printf("\n");
-}
-
-int	main(int ac, char **av)
-{
-	int	nb_fd;
-
-	if (ac > 1)
-	{
-		nb_fd = ac - 1;
-		test_gnl(nb_fd, av);
-	}
-	else
-		printf("please enter at list a file as argument for GNL to be tested\n");
+	close(fd1);
 	return (0);
 }
